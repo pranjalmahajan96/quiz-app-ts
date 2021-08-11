8a5e16da0738474a384e06cd6c5e2af2b28dbccf
Pranjal Mahajan <pranjalmahajan96@gmail.com>
2021-08-11 22:42:47 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { Route, Routes } from "react-router-dom";
+import { Quiz, Navbar } from "./components";
+
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Quiz />
+      <Routes>
+        {/* <Route path="/quiz" element={<Quiz />} /> */}
+      </Routes>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
+import { QuizProvider } from "./context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <QuizProvider>
+          <App />
+      </QuizProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
